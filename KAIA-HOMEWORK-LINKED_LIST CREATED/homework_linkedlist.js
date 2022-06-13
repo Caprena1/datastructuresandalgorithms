2e0028a3afc82f81d0b7dee70f1f98234d9e5768
XCA Student <xcastudent@Admins-MBP.attlocal.net>
2022-06-13 18:52:21 -0400

    modifications

--- KAIA-HOMEWORK-LINKED_LIST CREATED/homework_linkedlist.js
+++ KAIA-HOMEWORK-LINKED_LIST CREATED/homework_linkedlist.js
@@ -1,27 +1,48 @@
 // Homework: In a js file called linked_list.js, build a linked list and node class with out reference. Then, add 3 nodes to that linked list using a push class method.
 //LINKLIST WAS USED PRIOR TO ARRAYS
+
 class LinkedList {
+   
     constructor(head = null) {
         this.head = head
     }
     
-push(data){ 
-    let node = new Node(data)
-    let current
-    if(this.head === null) {
-        this.head = node
-    } else {
-        current.next = this.head
+    push(data) { 
+        let node = new Node(data)
+        let current
+        if(this.head == null) {
+            this.head = node
+        } else {
+            current = this.head
 
-        while(current.next){ 
-            current = current.next
-        
-        current.next = node
+            while(current.next){ 
+                current = current.next
+            
+            current.next = node
+            }
         }
     }
-    length++;
-    return this;
-}
+   
+    reverseIterative(node) {
+        let current = node
+        let prev = null
+        let next = null
+    
+        while(current != null){
+            next - current.next
+    
+            current.next = prev
+            
+            prev = current
+    
+            current = next
+        }
+    
+        node = prev
+        return node
+    
+    }
+
 }
 
 class Node {
@@ -31,41 +52,16 @@ class Node {
     }
 }
 
-let list = new LinkedList(node1)
 
-list.push(node1)
-list.push(node2)
-list.push(node3)
+let list = new LinkedList()
+
+list.push("Sherwood")
+list.push("Main")
+list.push("Taylor")
 console.log(list)
 
 
 
-reverse() {
-    let current = this.head
-    let previous = null
-    let next = head.next
-    while(current != null) {
-        current.next = previous
-        previous = current
-        current = next
-    }
-}
 
-reverseIterative(node) {
-    let current = node
-    let prev = null
-    let next = null
 
-    while(current != null){
-        next - current.next
 
-        current.next = prev
-        
-        prev = current
-
-        current = next
-    }
-
-    node = prev
-    return node
-}
